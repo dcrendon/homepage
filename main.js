5ed25992a45d3b45be58aacfe2ae29df138eda1d
dcrendon <daniel.rendonfc@gmail.com>
2023-11-23 22:19:47 -0600

    codelist animation

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ import 'animate.css';
 
 const codeIcon = document.getElementById('code')
 const codeList = document.getElementById('code-list')
+const nameIcons = document.getElementById('name-icons')
 
 codeIcon.addEventListener('click', () => {
-    codeList.classList.toggle('hidden')
+    if (codeList.classList.contains('animate__fadeOut')) {
+        nameIcons.style.transform = 'translateY(-10rem)';
+        setTimeout(() => {
+            codeList.classList.remove('animate__fadeOut');
+            codeList.classList.add('animate__fadeIn');
+            codeList.style.display = 'flex';
+        }, 250);
+    }else{
+        codeList.classList.remove('animate__fadeIn');
+        codeList.classList.add('animate__fadeOut');
+        setTimeout(() => {
+            codeList.style.display = 'none';
+            nameIcons.style.transform = 'translateY(0)';
+        }, 250);
+    }
 });
